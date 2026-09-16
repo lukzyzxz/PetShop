5df9c9d1a6cdcd97bc9c89285be43753b4014736
Lucas Felipe Ribeiro Queiroz <00001109958626sp@aluno.educacao.sp.gov.br>
2026-09-15 21:12:51 -0300

    feat

---
+++ ProjetoPetShop/Models/Tutor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoPetShop.Models
+{
+    public class Tutor
+    {
+        /*
+        create table Tutor(
+        id_tutor int primary key auto_increment,
+        nome varchar(67) not null,
+        telefone varchar(15) not null,
+        email varchar(49) not null
+        );
+
+        */
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string nome { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "O telefone é obrigatório")]
+        public string telefone { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "O email é obrigatório")]
+        public string email { get; set; }
+    }
+}
